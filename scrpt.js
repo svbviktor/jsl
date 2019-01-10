6180eabc74aca25b3c7c202471230a82fac03432
V <2429920@gmail.com>
2019-01-10 12:59:27 +0300

    C1

---
+++ scrpt.js
@@ -0,0 +1,32 @@
+let money = prompt("Тест", '');
+console.log(money);
+let date = prompt("Тест", '');
+console.log(date);
+let addData = {
+    butjet: money,
+    timeData: date,
+
+};
+
+
+
+
+
+
+
+
+let exType = prompt("Тест", '');
+
+let exVal = prompt("Тест", '');
+
+let expenses = {
+    exType: exVal
+};
+console.log(expenses);
+let optionalExpenses = {
+
+};
+
+
+let income = [];
+ let savings  = false;
